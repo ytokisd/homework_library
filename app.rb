e7f2843bf2b50e184f19f9ec8e8a42b94de24bbd
Yevhen Tokarenko <ytokisd@gmail.com>
2018-05-13 14:54:17 +0300

    changes according to code review

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "./library.rb"
 puts "Please type a library file name (e. g. lib_data.yml)"
 libdata = gets.chomp
 library = Library.new(libdata)
+
 loop do
   puts 'Welcome to my Library. Select a number to perform a task:
 1 - Display the most active Reader
@@ -13,20 +14,15 @@ loop do
 6 - Exit'
   number = gets.to_i
   case number
-  when 1
-  library.often_takes_book
-  when 2
-  library.most_popular_book
-  when 3
-  library.three_popular_books
+  when 1 then library.often_takes_book
+  when 2 then library.most_popular_book
+  when 3 then library.three_popular_books
   when 4
   puts "Please enter new file name (e. g. new.yml)"
   destination = gets.chomp
   library.save_to_file(destination)
-  when 5
-  library.load_data
-  when 6
-  break
+  when 5 then library.load_data
+  when 6 then break
   else
   "You should enter a number from the description above"
   end
